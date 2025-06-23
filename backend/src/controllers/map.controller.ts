649ea8bdf28b4a8f97aafdedc58e07d895a84db2
snnktri <skktri237@gmail.com>
2025-06-23 14:54:50 +0545

    ranjit map merged

--- backend/src/controllers/map.controller.ts
+++ backend/src/controllers/map.controller.ts
@@ -13,6 +13,7 @@ export const getRoute = asyncHandler(async(req: Request, res: Response)=>{
        throw new ApiError(400, "Coordinates required");
     }
 
+
     const startId = nearestNode(start, nodeMap);
     const goalId = nearestNode(end, nodeMap);
 
