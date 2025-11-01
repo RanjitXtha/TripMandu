896fa319d9e00c6c44c8c16aa701a05d054f4dcb
snnktri <skktri237@gmail.com>
2025-11-01 11:46:26 +0545

    changes

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,10 +1,8 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import type { RegisterForm } from "../types/user";
 import React, { useState } from "react";
 import { registerFormData } from "../apiHandle/user";
 import { useNavigate } from "react-router-dom";
-
-
 
 const Register = () => {
   const [registerData, setRegisterData] = useState<RegisterForm>({
@@ -58,107 +56,137 @@ const Register = () => {
     });
 
     try {
-        const response = await registerFormData(formData);
-        console.log(response);
-        if(response?.success) {
-            alert(response?.message);
-            navigate("/login");
-            setError(null);
-        }
+      const response = await registerFormData(formData);
+      console.log("kfdkl k: ", response);
 
+      if (response?.success) {
+        alert(response?.message);
+        navigate("/login");
+        setError(null);
+      }
     } catch (err: any) {
+      console.log(err.status);
+      if (err.status === 403) {
+        setError("Email already taken");
+      } else {
         setError(err?.message || "register failed");
-
-    }finally {
-        setLoading(false);
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div
-      className="w-full bg-gradient-to-r from-gray-200 to-gray-100 min-h-screen
-    flex items-center justify-center"
-    >
-      <div className="w-4/5 md:w-3/5 bg-white shadow-xl p-8 m-8">
-        <h2 className="text-xl text-center font-bold text-blue-600">
-          Register
-        </h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label className="text-sm text-gray-700">First Name:</label>
-            <input
-              type="text"
-              name="fName"
-              value={registerData.fName}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2
-                    rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-gray-700">Last Name:</label>
-            <input
-              type="text"
-              name="lName"
-              value={registerData.lName}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2
-                    rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-gray-700">Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={registerData.email}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2
-                    rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-gray-700">Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={registerData.password}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2
-                    rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-gray-700">Profile:</label>
-            <input
-              type="file"
-              name="profile"
-              accept="image/*"
-              onChange={handleFile}
-              className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full  bg-blue-500 rounded-xl py-2 text-white hover:bg-blue-600
+    <div className="flex flex-col h-screen">
+      {/* <div className="w-full p-4 bg-gray-300">
+        <Link to="/" className="text-3xl">
+          Map
+        </Link>
+      </div> */}
+      <div className="flex flex-1 w-full bg-gradient-to-r from-gray-400 to-gray-100 items-center justify-center">
+        <div className="w-4/5 md:w-3/5 bg-white shadow-xl p-8 m-8 rounded-3xl">
+          <h2 className="text-xl text-center font-bold text-blue-600">
+            Register
+          </h2>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="fName" className="text-sm text-gray-700">
+                First Name:
+              </label>
+              <input
+                id="fName"
+                type="text"
+                name="fName"
+                value={registerData.fName}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2
+                    rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="lName" className="text-sm text-gray-700">
+                Last Name:
+              </label>
+              <input
+                id="lName"
+                type="text"
+                name="lName"
+                value={registerData.lName}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2
+                    rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="text-sm text-gray-700">
+                Email:
+              </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={registerData.email}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2
+                    rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-sm text-gray-700">
+                Password:
+              </label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                minLength={8}
+                value={registerData.password}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2
+                    rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="profile" className="text-sm text-gray-700">
+                Profile:
+              </label>
+              <input
+                id="profile"
+                type="file"
+                name="profile"
+                accept="image/*"
+                onChange={handleFile}
+                className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full  bg-blue-500 rounded-xl py-2 text-white hover:bg-blue-600
            hover:scale-105 transform ease-in duration-75 delay-75
             cursor-pointer"
-          >
-           {loading ? "Registering..." : "Register"}
-          </button>
-          <p className="text-center">
-            Already have account?{" "}
-            <NavLink to="/login" className="text-blue-700">
-              Login
-            </NavLink>
-          </p>
-        </form>
-        {error && <p className="text-center text-red-500">{error}</p>}
+            >
+              {loading ? "Registering..." : "Register"}
+            </button>
+            <p className="text-center">
+              Already have account?{" "}
+              <NavLink to="/login" className="text-blue-700">
+                Login
+              </NavLink>
+            </p>
+            <p className="text-center">
+              Or,{" "}
+              <Link to="/" className=" hover:underline">
+                visit map without registering.
+              </Link>
+            </p>
+          </form>
+          {error && <p className="text-center text-red-500">{error}</p>}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { LoginFrom } from "../types/user";
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { LoginFromData } from "../apiHandle/user";
 import type { User } from "../types/user";
 import { setUser } from "../features/auth";
@@ -36,7 +36,7 @@ const SignIn = () => {
     setError(null);
 
     try {
-      const response = await LoginFromData(loginData); // pass loginData here
+      const response = await LoginFromData(loginData);
 
       if (response?.success) {
         console.log(response);
@@ -51,60 +51,86 @@ const SignIn = () => {
         alert(response.message || "Login successful");
 
         navigate("/");
-        setError(null);
-      } else {
-        setError(response?.message || "Login failed");
       }
     } catch (err: any) {
-      setError(err?.message || "Login failed");
+      console.log(err);
+      if (err.status === 404) {
+        setError("User not found");
+      } else if (err.status === 403) {
+        setError("Incorrect password.");
+      } else {
+        setError(err?.message || "Login failed");
+      }
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="w-full bg-gradient-to-r from-gray-200 to-gray-100 min-h-screen flex items-center justify-center">
-      <div className="w-4/5 md:w-3/5 bg-white shadow-xl p-8 m-8">
-        <h2 className="text-xl text-center font-bold text-blue-600">Login</h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label className="text-sm text-gray-700">Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={loginData.email}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-gray-700">Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={loginData.password}
-              onChange={handleInput}
-              required
-              className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+    <div className="flex flex-col h-screen">
+      {/* <div className="w-full p-4 bg-gray-400">
+        <Link to="/" className="text-3xl hover:underline">
+          Visit map without logging in
+        </Link>
+      </div> */}
 
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full bg-blue-500 rounded-xl py-2 text-white hover:bg-blue-600 hover:scale-105 transform ease-in duration-75 delay-75 cursor-pointer"
-          >
-            {loading ? "Logging in..." : "Login"}
-          </button>
-          <p className="text-center">
-            Don’t have an account?{" "}
-            <NavLink to="/register" className="text-blue-700">
-              Register
-            </NavLink>
-          </p>
-        </form>
-        {error && <p className="text-center text-red-500 mt-2">{error}</p>}
+      <div className="flex flex-1 w-full bg-gradient-to-r from-gray-400 to-gray-100 items-center justify-center">
+        <div className="w-3/5 md:w-3/5 bg-white shadow-xl p-8 m-4 rounded-3xl">
+          <h2 className="text-xl text-center font-bold text-blue-600">Login</h2>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="text-sm text-gray-700">
+                Email:
+              </label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={loginData.email}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-sm text-gray-700">
+                Password:
+              </label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                minLength={8}
+                value={loginData.password}
+                onChange={handleInput}
+                required
+                className="w-full border border-gray-500 px-2 py-2 mt-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full bg-blue-500 rounded-xl py-2 text-white hover:bg-blue-600 hover:scale-105 transform ease-in duration-75 delay-75 cursor-pointer"
+            >
+              {loading ? "Logging in..." : "Login"}
+            </button>
+            <p className="text-center">
+              Don&apos;t have an account?{" "}
+              <NavLink to="/register" className="text-blue-700">
+                Register
+              </NavLink>
+            </p>
+            <p className="text-center">
+              Or,{" "}
+              <Link to="/" className=" hover:underline text-blue-500 italic">
+                visit map
+              </Link>
+              {" "} without logging in.
+            </p>
+          </form>
+          {error && <p className="text-center text-red-500 mt-2">{error}</p>}
+        </div>
       </div>
     </div>
   );
